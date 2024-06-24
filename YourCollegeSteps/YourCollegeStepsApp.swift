b8576ee8e9562812e832f54a97ee6cc474aeeb52
MNaccaratto <mnaccaratto@gmail.com>
2024-06-24 10:15:20 -0400

    Initial Commit

---
+++ YourCollegeSteps/YourCollegeStepsApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourCollegeStepsApp.swift
+//  YourCollegeSteps
+//
+//  Created by Lauren Naccaratto on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct YourCollegeStepsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
