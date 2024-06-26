4856172a3868d7f9fc13b4f0d87a6e298078c47e
MNaccaratto <mnaccaratto@gmail.com>
2024-06-26 13:40:12 -0400

    Big View File Update

---
+++ YourCollegeSteps/PSATInfoView.swift
@@ -0,0 +1,18 @@
+//
+//  PSATInfoView.swift
+//  YourCollegeSteps
+//
+//  Created by Lauren Naccaratto on 6/26/24.
+//
+
+import SwiftUI
+
+struct PSATInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PSATInfoView()
+}
